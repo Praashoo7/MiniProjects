eca5b1124e8cfefed09b7e34da225fcbf7148e55
Prashant Patil <rnttl90@gmail.com>
2022-02-08 16:10:01 +0530

    Update chars.py

--- TypeSpeed/chars.py
+++ TypeSpeed/chars.py
@@ -1,4 +1,4 @@
-import charsMod as cm
+import charsModule as cm
 
 def main():
 
@@ -6,15 +6,11 @@ def main():
     print("\n")
     
     print("Press <ENTER> after You're finished typing..\n")
+    
     try:
-        n=int(input("To Start Typing, Enter 1 and <ENTER> and Start Typing..\n\n"))
-        if n==1:
-            print()
-            cm.charsMod()
-        else:
-            print("\nINVALID INPUT")
-    except Exception:
-        print("\nINVALID INPUT")
-  
+        cm.charsMod()
+    except Exception as c:
+        print("\n",c)
+
 if __name__=="__main__":
 	main()
