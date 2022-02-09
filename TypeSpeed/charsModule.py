c4b2de77746b65fe944acfe6dbe6a30a3da2da25
Prashant Patil <rnttl90@gmail.com>
2022-02-10 00:33:00 +0530

    Create charsModule.py

---
+++ TypeSpeed/charsModule.py
@@ -0,0 +1,85 @@
+import time
+import pandas as pd
+import openpyxl
+from openpyxl import Workbook
+
+def charsMod():	
+    
+    alph="abcdefghijklmnopqrstuvwxyz"
+    
+    name=input("Enter Your Name : ")
+
+    data=pd.read_excel("TypeBoard.xlsx")
+    namelist=data["Name"].tolist()
+    
+    for n in namelist:
+        if n==name:
+            print("\nName Already Exists\n")
+            exit()
+    
+    n=int(input("\nTo Start Typing, Enter 1 and <ENTER> and Start Typing..\n\n"))
+    
+    if n==1:
+    
+        s=time.time()
+        str=input("")
+        string=str.lower()
+    
+        if string==alph:
+            print("\nSuccess..")
+            e=time.time()
+            t=e-s
+            print("Time is : ",e-s)
+            print("WPM : ",(26/5)/((e-s)/60))
+            print()
+            ld(name,t)
+            exit()
+        else:
+            print("\nWRONG CHARACHTER")
+
+    else:
+        
+        print("\nINVALID INPUT")
+        
+def ld(n1,t1):
+
+    name=n1
+    time=t1
+    dict={}
+    sdict={}
+    namelist=[]
+    timelist=[]
+    
+    ob=openpyxl.load_workbook("TypeBoard.xlsx")
+    ba=ob.active
+    
+    for i in range(1,100):
+        if ba.cell(row=i,column=1).value is None and ba.cell(row=i,column=2).value is None:
+            ba.cell(row=i,column=1).value=name
+            ba.cell(row=i,column=2).value=time
+            break
+    
+    ob.save(filename="TypeBoard.xlsx")
+    
+    data=pd.read_excel("TypeBoard.xlsx")
+    namelist=data["Name"].tolist()
+    timelist=data["Time"].tolist()
+
+    for i in range(len(namelist)):
+        dict[namelist[i]]=timelist[i]
+    
+    sortv=sorted(dict.values())
+    
+    for i in sortv:
+        for j in dict.keys():
+            if dict[j]==i:
+                sdict[j]=dict[j]
+                break
+                
+    print("\nTOP 5\n")
+    
+    i=0
+    for key in sdict:
+        if i!=5:
+            print(key,":",sdict[key])
+            i=i+1    
